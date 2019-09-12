2804c18666b9b6a3aebeccc5dffc220ede3b1256
Frank Hartmann <frank.hartmann@dreamchip.de>
2019-09-12 16:11:45 +0200

    timestamp: add aarch64

--- timestamp.h
+++ timestamp.h
@@ -10,7 +10,7 @@
 #include <inttypes.h>
 #include <time.h>
 
-#if defined(__ARCH_GENERIC__)
+#if defined(__ARCH_GENERIC__) && !defined(__aarch64__)
 #define TIMESTAMP_NOT_CLOCK_CYCLE_COUNTER
 static inline void
 timestamp(volatile uint64_t * volatile ts)
@@ -51,5 +51,17 @@ timestamp(volatile uint64_t *ts)
 	: "0"(((volatile uint32_t *)ts)[0]), "1"(((volatile uint32_t *)ts)[1]));
 }
 #endif
-
+#if defined(__aarch64__)
+static inline void
+timestamp(volatile uint64_t * volatile ts)
+{
+  // System timer of ARMv8 runs at a different frequency than the CPU's.
+  // The frequency is fixed, typically in the range 1-50MHz.  It can be
+  // read at CNTFRQ special register.  We assume the OS has set up
+  // the virtual timer properly.
+  int64_t virtual_timer_value;
+  asm volatile("mrs %0, cntvct_el0" : "=r"(virtual_timer_value));
+  *ts = virtual_timer_value;
+}
+#endif
 #endif
